29afbc3e574e08b4750a7acafbe18a220f8bb747
Soha Rida khan <soharida2005@gmail.com>
2026-02-04 20:48:02 +0530

    Count of Matches in Tournament

---
+++ math/match_tournament.py
@@ -0,0 +1,8 @@
+'''
+1688. Count of Matches in Tournament
+https://leetcode.com/problems/count-of-matches-in-tournament/description/
+'''
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
+        
