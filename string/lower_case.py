b1f439cc6ed250bb214bd85efef857c889527729
Soha Rida khan <soharida2005@gmail.com>
2026-02-11 22:13:28 +0530

    to lower case

---
+++ string/lower_case.py
@@ -0,0 +1,8 @@
+"""
+709. To Lower Case
+link: https://leetcode.com/problems/to-lower-case/description/
+"""
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s=s.lower()
+        return s
