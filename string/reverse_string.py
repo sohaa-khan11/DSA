4eb6cfe3b0a17d95d5110b0e6470cc9e6988008b
Soha Rida khan <soharida2005@gmail.com>
2026-02-12 13:46:10 +0530

    344. reverse string

---
+++ string/reverse_string.py
@@ -0,0 +1,11 @@
+"""
+344. Reverse String
+link : https://leetcode.com/problems/reverse-string/description/
+"""
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s=s.reverse()
+        return s
