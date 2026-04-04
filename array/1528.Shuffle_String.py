178f5884af2ce5e68030a0ae0f4ec3849528865a
Soha Rida khan <soharida2005@gmail.com>
2026-04-04 18:48:25 +0530

    1528. Shuffle String

---
+++ array/1528.Shuffle_String.py
@@ -0,0 +1,12 @@
+"""
+1528. Shuffle String
+link: https://leetcode.com/problems/shuffle-string/
+"""
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result=[''] * len(s)
+        for i in range(len(s)):
+            result[indices[i]] = s[i]
+        
+        return ''.join(result)
+        
