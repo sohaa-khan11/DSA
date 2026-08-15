45669cfc43c250e9cf7b233a4ad0c444ebd96953
Soha Rida khan <soharida2005@gmail.com>
2026-08-15 11:39:17 +0530

    268.missing_numbers

---
+++ array/268.missing_numbers.py
@@ -0,0 +1,10 @@
+"""
+268. Missing Numbers
+link : https://leetcode.com/problems/missing-number/description/
+"""
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length=len(nums)
+        for i in range (0, length+1):
+            if i not in nums:
+                return i
